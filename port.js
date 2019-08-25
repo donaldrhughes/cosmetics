7983ce602357e91a8b85906bb9d9c210c0c6742a
myrean <dhughes1@uci.edu>
2019-08-24 22:14:19 -0700

    added uriBase const

--- port.js
+++ port.js
@@ -14,26 +14,31 @@ class Port extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:3001/api/server/port')
+        const uriBase = process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3001/'
+            : 'https://protected-harbor-72820.herokuapp.com/'
+        axios.get(uriBase + 'api/server/port')
             .then((result) => {
-               
+
                 const PORT = result.data.port;
                 const url = result.data.url;
-                this.setState({ port: PORT, url: url
-                  
+                this.setState({
+                    port: PORT, url: url
+
                 });
                 this.props = PORT;
-                
+
             });
     }
 
     render() {
 
         return (
-    
+
                     // <Text>PORT:{this.state.port}</Text>
                     <Text>url:{this.state.url}</Text>
-                    // <Text>data:{this.data}</Text>
+
+                    // <Text>url:{process.env.NODE_ENV}</Text>
             
         );
     }
